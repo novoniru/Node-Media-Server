16b3bfc0b326de8dc2cd918849f54b10dac6ae10
Stephanie Muhammad <srmuhammad70@gmail.com>
2020-09-20 19:59:02 -0400

    test

--- node_media_server.js
+++ node_media_server.js
@@ -4,14 +4,14 @@
 //  Copyright (c) 2018 Nodemedia. All rights reserved.
 //
 
-const Https = require('https');
-const Logger = require('./node_core_logger');
-const NodeRtmpServer = require('./node_rtmp_server');
-const NodeHttpServer = require('./node_http_server');
-const NodeTransServer = require('./node_trans_server');
-const NodeRelayServer = require('./node_relay_server');
-const NodeFissionServer = require('./node_fission_server');
-const context = require('./node_core_ctx');
+const Https = require("https");
+const Logger = require("./node_core_logger");
+const NodeRtmpServer = require("./node_rtmp_server");
+const NodeHttpServer = require("./node_http_server");
+const NodeTransServer = require("./node_trans_server");
+const NodeRelayServer = require("./node_relay_server");
+const NodeFissionServer = require("./node_fission_server");
+const context = require("./node_core_ctx");
 const Package = require("./package.json");
 
 class NodeMediaServer {
@@ -22,6 +22,7 @@ class NodeMediaServer {
   run() {
     Logger.setLogType(this.config.logType);
     Logger.log(`Node Media Server v${Package.version}`);
+    Logger.log("Note: This is a fork.");
     if (this.config.rtmp) {
       this.nrs = new NodeRtmpServer(this.config);
       this.nrs.run();
@@ -34,7 +35,7 @@ class NodeMediaServer {
 
     if (this.config.trans) {
       if (this.config.cluster) {
-        Logger.log('NodeTransServer does not work in cluster mode');
+        Logger.log("NodeTransServer does not work in cluster mode");
       } else {
         this.nts = new NodeTransServer(this.config);
         this.nts.run();
@@ -43,7 +44,7 @@ class NodeMediaServer {
 
     if (this.config.relay) {
       if (this.config.cluster) {
-        Logger.log('NodeRelayServer does not work in cluster mode');
+        Logger.log("NodeRelayServer does not work in cluster mode");
       } else {
         this.nls = new NodeRelayServer(this.config);
         this.nls.run();
@@ -52,36 +53,43 @@ class NodeMediaServer {
 
     if (this.config.fission) {
       if (this.config.cluster) {
-        Logger.log('NodeFissionServer does not work in cluster mode');
+        Logger.log("NodeFissionServer does not work in cluster mode");
       } else {
         this.nfs = new NodeFissionServer(this.config);
         this.nfs.run();
       }
     }
 
-    process.on('uncaughtException', function (err) {
-      Logger.error('uncaughtException', err);
+    process.on("uncaughtException", function (err) {
+      Logger.error("uncaughtException", err);
     });
 
     Https.get("https://registry.npmjs.org/node-media-server", function (res) {
       let size = 0;
       let chunks = [];
-      res.on('data', function (chunk) {
+      res.on("data", function (chunk) {
         size += chunk.length;
         chunks.push(chunk);
       });
-      res.on('end', function () {
+      res.on("end", function () {
         let data = Buffer.concat(chunks, size);
         let jsonData = JSON.parse(data.toString());
-        let latestVersion = jsonData['dist-tags']['latest'];
-        let latestVersionNum = latestVersion.split('.')[0] << 16 | latestVersion.split('.')[1] << 8 | latestVersion.split('.')[2] & 0xff;
-        let thisVersionNum = Package.version.split('.')[0] << 16 | Package.version.split('.')[1] << 8 | Package.version.split('.')[2] & 0xff
+        let latestVersion = jsonData["dist-tags"]["latest"];
+        let latestVersionNum =
+          (latestVersion.split(".")[0] << 16) |
+          (latestVersion.split(".")[1] << 8) |
+          (latestVersion.split(".")[2] & 0xff);
+        let thisVersionNum =
+          (Package.version.split(".")[0] << 16) |
+          (Package.version.split(".")[1] << 8) |
+          (Package.version.split(".")[2] & 0xff);
         if (thisVersionNum < latestVersionNum) {
-          Logger.log(`There is a new version ${latestVersion} that can be updated`);
+          Logger.log(
+            `There is a new version ${latestVersion} that can be updated`
+          );
         }
       });
-    }).on('error', function (e) {
-    });
+    }).on("error", function (e) {});
   }
 
   on(eventName, listener) {
@@ -108,4 +116,4 @@ class NodeMediaServer {
   }
 }
 
-module.exports = NodeMediaServer
+module.exports = NodeMediaServer;
